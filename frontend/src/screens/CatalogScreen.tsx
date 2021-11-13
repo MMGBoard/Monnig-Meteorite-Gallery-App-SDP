26d7f8c1bfe5c2464ce06664fc6529cfa9bf74c8
Kendricdspain <kendric.dspain@tcu.edu>
2021-11-12 20:52:07 -0600

    Fixed bugs on CatalogScreen.tsx screen

--- frontend/src/screens/CatalogScreen.tsx
+++ frontend/src/screens/CatalogScreen.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import { Text, View } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Searchbar} from 'react-native-paper';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import RNPickerSelect from 'react-native-picker-select';
 
-export default function CatalogScreen({navigation} : {navigation: any}) {
-    const [searchQuery, setSearchQuery] = React.useState('');
-    const onChangeSearch = query => setSearchQuery(query);
+export default function CatalogScreen({navigation}: {navigation: any}) {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = (query: React.SetStateAction<string>) =>
+    setSearchQuery(query);
 
-    return (
-      <View style={{ }}>
-          <Searchbar
-              placeholder="Find by meteorite"
-              onChangeText={onChangeSearch}
-              value={searchQuery}
-              icon={()=><MaterialCommunityIcon name="barcode-scan" size={30}/>}
-          />
-          <RNPickerSelect style={{width: "30%" }}
-              onValueChange={(value) => console.log(value)}
-              items={[
-                  { label: 'By Display', value: 'by display' },
-                  { label: 'ALL', value: 'all' }
-              ]}
-          />
-      </View>
-    );
+  return (
+    <View style={{}}>
+      <Searchbar
+        placeholder="Find by meteorite"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        icon={() => <MaterialCommunityIcon name="barcode-scan" size={30} />}
+      />
+      <RNPickerSelect
+        onValueChange={value => console.log(value)}
+        items={[
+          {label: 'By Display', value: 'by display'},
+          {label: 'ALL', value: 'all'},
+        ]}
+      />
+    </View>
+  );
 }
