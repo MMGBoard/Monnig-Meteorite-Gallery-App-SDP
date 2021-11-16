49e945efcb31ce3a114ae3a346dbf83091be33f0
ataddesse <amanueltesfaye44@gmail.com>
2021-11-15 18:48:20 -0600

    Some UI fixes, Firestore data import, and Firestore querying

--- frontend/src/screens/AcuityScreen.tsx
+++ frontend/src/screens/AcuityScreen.tsx
@@ -1,35 +1,37 @@
 import React, { useState } from 'react'
 import { Text, View, Button, StyleSheet } from 'react-native';
-import { RadioButton, Text as TextPaper } from 'react-native-paper';
+import { RadioButton, Button as PaperButton, Text as TextPaper } from 'react-native-paper';
 
 export default function AcuityScreen({navigation} : {navigation: any}) {
   const [flexDirection, setflexDirection] = useState("column");
   const [value, setValue] = React.useState('first');
   return (
     <View style={styles.container}>
+    <View style={styles.backButton}>
+          <PaperButton 
+            icon="chevron-left" mode="contained" color="#4D1979"
+            onPress={() => navigation.navigate('AcuityScreen')}
+          ><Text>Back</Text></PaperButton>
+       </View>
       <TextPaper style={styles.label}>Please select your prefered font-size:</TextPaper>
       <View style={styles.radioContainer}>
         <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
-            <RadioButton.Item labelStyle={{fontSize: 18, fontFamily: 'ROBOTO'}} label="Welcome to TCU!" value="firstChoice" />
-            <RadioButton.Item labelStyle={{fontSize: 24, fontFamily: 'ROBOTO'}} label="Welcome to TCU!" value="secondChoice" />
-            <RadioButton.Item labelStyle={{fontSize: 36, fontFamily: 'ROBOTO'}} label="Welcome to TCU!" value="thirdChoice" />
-            <RadioButton.Item labelStyle={{fontSize: 48, fontFamily: 'ROBOTO'}} label="Welcome to TCU!" value="fourthChoice" />
+            <RadioButton.Item labelStyle={{fontSize: 18, fontFamily: 'ROBOTO'}}
+              color="#4D1979" label="Welcome to TCU!" value="firstChoice" />
+            <RadioButton.Item labelStyle={{fontSize: 24, fontFamily: 'ROBOTO'}} 
+              color="#4D1979" label="Welcome to TCU!" value="secondChoice" />
+            <RadioButton.Item labelStyle={{fontSize: 36, fontFamily: 'ROBOTO'}}
+              color="#4D1979" label="Welcome to TCU!" value="thirdChoice" />
+            <RadioButton.Item labelStyle={{fontSize: 48, fontFamily: 'ROBOTO'}}
+              color="#4D1979" label="Welcome to TCU!" value="fourthChoice" />
         </RadioButton.Group>
       </View>
-      <View style={styles.buttons}>
-        <View style={styles.backButton}>
-          <Button
-            title="Back" color="#4D1979"
-            onPress={() => navigation.navigate('AcuityScreen')}
-          />
-       </View>
        <View style={styles.nextButton}>  
           <Button
             title="Next" color="#4D1979"
             onPress={() => navigation.navigate('ColorBlindnessScreen')}
           />
        </View>  
-       </View>
     </View>
   );
 }
@@ -37,41 +39,32 @@ export default function AcuityScreen({navigation} : {navigation: any}) {
 const styles = StyleSheet.create({
   label: {
     textAlign: "left",
-    marginTop: 50,
-    marginBottom: 10,
-    marginLeft: 10,
     marginRight: 10,
     fontSize: 24,
-    fontFamily: 'ROBOTO'
+    fontFamily: 'ROBOTO',
+    marginBottom: 30
   },
   container: {
     flex: 1,
     justifyContent: 'center',
+    marginLeft: 30,
+    marginRight: 30,
   },
   radioContainer: {
     flex: 1,
-    marginTop: 50,
-  },
-  buttons: {
-    flexDirection: "row",
-    flex: 1,
-    marginTop: 200,
-    marginLeft: 20,
-    marginRight: 20, 
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    bottom:0
   },
   backButton: {
-    flexDirection: "row",
     flex: 1,
-    justifyContent: 'flex-start',
-    bottom:0
+    marginTop: 30,
+    marginLeft: 10,
+    width: "40%"
   },
   nextButton: {
-    flexDirection: "row",
     flex: 1,
-    justifyContent: 'flex-end',
+    marginTop: 250,
+    marginBottom: 50,
+    width: "60%",
+    alignSelf: 'center',
     bottom:0
   },
 });
--- frontend/src/screens/CatalogScreen.tsx
+++ frontend/src/screens/CatalogScreen.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import RNPickerSelect from 'react-native-picker-select';
+import firestore from '@react-native-firebase/firestore';
 
 export default function CatalogScreen({navigation} : {navigation: any}) {
-    const [searchQuery, setSearchQuery] = React.useState('');
-    const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
 
-    return (
+    const data = firestore()
+        .collection('meteorites')
+        // Filter results
+        .where('COUNTRY', '==', 'USA')
+        .get().then(querySnapshot => {
+            console.log("No of meteorites found in USA: " + querySnapshot.docs.length);
+        });
+    
+    return (   
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>CBCheck!</Text>
-        <Button
-        title="Start Tour!"
-        onPress={() => navigation.navigate('TabNavigator')}
-      />
+        <Text>CatalogScreen!</Text>
     </View>
     );
 }
